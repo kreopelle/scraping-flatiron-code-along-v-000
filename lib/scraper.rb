c1cb9fbb8462a6b130d02fb1491494ca129eb1ae
Kayla Reopelle <kreopelle@gmail.com>
2018-06-01 19:22:05 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,16 +12,16 @@ class Scraper
       binding.pry
     end
 
-    def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+#    def print_courses
+#    self.make_courses
+#    Course.all.each do |course|
+#      if course.title
+#        puts "Title: #{course.title}"
+#        puts "  Schedule: #{course.schedule}"
+#        puts "  Description: #{course.description}"
+#      end
+#    end
+#  end
 
 end
 
